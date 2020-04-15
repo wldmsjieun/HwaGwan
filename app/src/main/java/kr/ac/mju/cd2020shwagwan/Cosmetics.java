87392009b11177aeb331fc57ba721e37d504e7bc
wldmsjieun <nuei6625@naver.com>
2020-04-15 15:43:58 +0900

    Backend Frame base2

--- app/src/main/java/kr/ac/mju/cd2020shwagwan/Cosmetics.java
+++ app/src/main/java/kr/ac/mju/cd2020shwagwan/Cosmetics.java
@@ -1,14 +1,19 @@
 package kr.ac.mju.cd2020shwagwan;
 
 public class Cosmetics {
+    // id
+    private int id;
 
     private String itemBrand;
     private String itemName;
 
     public Cosmetics(int id, String itemBrand, String itemName) {
-
+        this.id = id;
         this.itemBrand = itemBrand;
         this.itemName = itemName;
+    }
+    public int getId() {
+        return this.id;
     }
 
     public String getitemBrand() {
