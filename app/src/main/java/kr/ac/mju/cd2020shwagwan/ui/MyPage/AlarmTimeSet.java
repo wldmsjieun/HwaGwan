c34f9290acf0d48381e4abe898fd22726baa994a
yujin610 <leesemi1215@naver.com>
2020-05-14 15:43:39 +0900

    알람 시각 저장

--- app/src/main/java/kr/ac/mju/cd2020shwagwan/ui/MyPage/AlarmTimeSet.java
+++ app/src/main/java/kr/ac/mju/cd2020shwagwan/ui/MyPage/AlarmTimeSet.java
@@ -3,17 +3,22 @@ package kr.ac.mju.cd2020shwagwan.ui.MyPage;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 
 import kr.ac.mju.cd2020shwagwan.R;
 
@@ -21,62 +26,31 @@ import static android.content.Context.MODE_PRIVATE;
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class AlarmTimeSet extends Dialog {
-    static public TextView tvHour;
-    static public TextView tvMinute;
-    int setHour=22, setMin=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //다이얼로그 밖의 화면은 흐리게 만들어줌
-        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
-        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-        layoutParams.dimAmount = 0.8f;
-        getWindow().setAttributes(layoutParams);
+        // WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        // layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        // layoutParams.dimAmount = 0.8f;
+        //getWindow().setAttributes(layoutParams);
 
-        setContentView(R.layout.alarm_time_set);
+//        setContentView(R.layout.alarm_time_set);
 
-        setTime();
+//        setTime();
+        showAddDialog();
 
-        
     }
 
     void setTime() {
-        LinearLayout timeSetButton = (LinearLayout) findViewById(R.id.dialog_alarm_time_set_button);
-        Button btnSave = findViewById(R.id.btnSave);
-        tvHour = (TextView) findViewById(R.id.dialog_hour);
-        tvMinute = (TextView) findViewById(R.id.dialog_minute);
 
-        final SharedPreferences sp = getContext().getSharedPreferences("alarmTime", MODE_PRIVATE);
+    }
 
-        timeSetButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                     tvHour.setText(Integer.toString(hourOfDay));
-                     tvMinute.setText(Integer.toString(minute));
-                     setHour = hourOfDay;
-                     setMin = minute;
+    public void showAddDialog() {
 
-                    }
-                },  Integer.parseInt(tvHour.getText().toString()), Integer.parseInt(tvMinute.getText().toString()),true);
 
-                timePickerDialog.show();
-            }
-        });
-
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-             SharedPreferences.Editor editor = sp.edit();
-             editor.putInt("hour", setHour);
-             editor.putInt("minute", setMin);
-             editor.commit();
-            }
-        });
     }
 
     public AlarmTimeSet(@NonNull Context context) {
--- app/src/main/java/kr/ac/mju/cd2020shwagwan/ui/MyPage/MypageFragment.java
+++ app/src/main/java/kr/ac/mju/cd2020shwagwan/ui/MyPage/MypageFragment.java
@@ -1,14 +1,23 @@
 package kr.ac.mju.cd2020shwagwan.ui.MyPage;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
@@ -16,26 +25,43 @@ import androidx.lifecycle.ViewModelProviders;
 
 import kr.ac.mju.cd2020shwagwan.R;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class MypageFragment extends Fragment {
 
     private MypageViewModel mypageViewModel;
 
-    private View mRoot;
+    private View mRoot, alarm_time_set;
 
     private AlarmTimeSet mAlarmTimeSetDialog;
+
+    static public TextView tvHour;
+    static public TextView tvMinute;
+    int setHour=22, setMin=0;
+    SharedPreferences sp;
+    View layout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mypageViewModel =
                 ViewModelProviders.of(this).get(MypageViewModel.class);
         mRoot = inflater.inflate(R.layout.fragment_mypage, container, false);
+        layout = inflater.inflate(R.layout.alarm_time_set, null);
 
+        sp = getContext().getSharedPreferences("alarmTime", MODE_PRIVATE);
+
+        tvHour = layout.findViewById(R.id.dialog_hour);
+        tvMinute = layout.findViewById(R.id.dialog_minute);
+//        alarm_time_set = inflater.inflate(R.layout.alarm_time_set, container, false);
 //        notificationsViewModel.getText().observe(this, new Observer<String>() {
 //            @Override
 //            public void onChanged(@Nullable String s) {
 //
 //            }
 //        });
+
+        tvHour.setText(String.valueOf(sp.getInt("hour", 22)));
+        tvMinute.setText(String.valueOf(sp.getInt("minute", 00)));
 
         setSwitch();
 
@@ -66,7 +92,52 @@ public class MypageFragment extends Fragment {
         alarmTimeSet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mAlarmTimeSetDialog.show();
+                // AlertDialog View layout
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                LinearLayout timeSetButton = layout.findViewById(R.id.dialog_alarm_time_set_button);
+
+                timeSetButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(), new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                                tvHour.setText(Integer.toString(hourOfDay));
+                                tvMinute.setText(Integer.toString(minute));
+                                setHour = hourOfDay;
+                                setMin = minute;
+
+                            }
+                        },  Integer.parseInt(tvHour.getText().toString()), Integer.parseInt(tvMinute.getText().toString()),true);
+
+                        timePickerDialog.show();
+
+                    }
+                });
+
+                if (layout.getParent() != null){
+                    ((ViewGroup) layout.getParent()).removeView(layout);
+                }
+
+                new AlertDialog.Builder(getContext(), R.style.MyAlertDialogStyle)
+                        .setPositiveButton("SAVE", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(@NonNull DialogInterface dialog, int which) {
+                                SharedPreferences.Editor editor = sp.edit();
+                                Log.d("save hour", String.valueOf(setHour));
+                                Log.d("save minute", String.valueOf(setMin));
+                                editor.putInt("hour", setHour);
+                                editor.putInt("minute", setMin);
+                                editor.commit();
+                            }
+                        })
+                        .setCancelable(true)
+                        .setTitle("현재 설정된 알림 시각")
+                        .setView(layout)
+                        .show();
+
+
             }
         });
     }
